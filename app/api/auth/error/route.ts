77e580b0d4f65e1e3dee48eb71b7e4be0f169372
Drew ✦ Andersen <drew@monsterswithink.com>
2025-07-23 11:07:32 -0600

    Update route.ts

--- app/api/auth/error/route.ts
+++ app/api/auth/error/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse }, type { NextRequest } from "next/server"
-/*import type { NextRequest } from "next/server" */
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
 /**
  * NextAuth still points to `/api/auth/error`.

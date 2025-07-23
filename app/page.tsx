81107fc2c363f67e2a0793593f92955692a8b2a5
Drew ✦ Andersen <drew@monsterswithink.com>
2025-07-23 07:17:22 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/ui/card"
 import { Eye, Edit, Linkedin } from "lucide-react"
 
 export default function HomePage() {

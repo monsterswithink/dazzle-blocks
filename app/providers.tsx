f301d06bb5631e569eded3481b08ac874cada5e1
Drew ✦ Andersen <drew@monsterswithink.com>
2025-07-31 16:47:08 -0600

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,21 @@
 
 import type React from "react"
 import { SessionProvider } from "next-auth/react"
+import { VeltProvider } from "@veltdev/react"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>
+  const veltApiKey = process.env.NEXT_PUBLIC_VELT_PUBLIC_KEY
+  return (
+    <SessionProvider>
+      <VeltProvider apiKey={veltApiKey}>{children}</VeltProvider>
+    </SessionProvider>
+  )
 }
+// "use client"
+
+// import type React from "react"
+// import { SessionProvider } from "next-auth/react"
+
+// export function Providers({ children }: { children: React.ReactNode }) {
+//   return <SessionProvider>{children}</SessionProvider>
+// }

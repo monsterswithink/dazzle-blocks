deecd1500b04ac2a0d2bd097abc79f3c0c847de5
Drew ✦ Andersen <drew@monsterswithink.com>
2025-08-02 01:35:47 -0600

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,7 @@
 import type React from "react";
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { VeltProvider } from "@/components/velt-provider";
+import { VeltProvider } from "@/resume-providers/Velt";
 import { useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {

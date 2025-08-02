29b322f7b24b3fc64bcb9117650458fa0d140a31
Drew ✦ Andersen <drew@monsterswithink.com>
2025-08-02 01:40:09 -0600

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,27 +1,12 @@
-"use client"
-
-import type React from "react";
-import { SessionProvider } from "next-auth/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { VeltProvider } from "@/resume-providers/Velt";
-import { useState } from "react";
+import type React from "react"
+import { SessionProvider } from "next-auth/react"
+import { VeltProvider } from "@veltdev/react"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() =>
-    new QueryClient({
-      defaultOptions: {
-        queries: {
-          staleTime: 60 * 1000, // 1 minute
-        },
-      },
-    })
-  );
-
+  const veltApiKey = process.env.NEXT_PUBLIC_VELT_PUBLIC_KEY
   return (
     <SessionProvider>
-      <QueryClientProvider client={queryClient}>
-        <VeltProvider>{children}</VeltProvider>
-      </QueryClientProvider>
+      <VeltProvider apiKey={VELT_PUBLIC_KEY}>{children}</VeltProvider>
     </SessionProvider>
-  );
+  )
 }

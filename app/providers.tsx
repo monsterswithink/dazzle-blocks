2f58aed2d36c98a9316830674c31eff2704dcd67
Drew ✦ Andersen <drew@monsterswithink.com>
2025-08-02 14:30:27 -0600

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,15 +3,18 @@
 import { SessionProvider } from "next-auth/react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes"
-import { Toaster } from "@/components/ui/toaster" // Assuming you have a Toaster component
+import { Toaster } from "@/components/ui/toaster"
+import { VeltProvider } from "@/components/resume-provider/Velt"
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
     <SessionProvider>
-      <NextThemesProvider {...props}>
-        {children}
-        <Toaster />
-      </NextThemesProvider>
+      <VeltProvider documentId="resume-app-root">
+        <NextThemesProvider {...props}>
+          {children}
+          <Toaster />
+        </NextThemesProvider>
+      </VeltProvider>
     </SessionProvider>
   )
 }

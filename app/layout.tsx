e809f59f4c118f164f2946aa3ba5eeff8d906c67
Drew ✦ Andersen <drew@monsterswithink.com>
2025-08-03 10:22:37 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Suspense } from "react"
-import { Providers } from "@/components/providers"
+import { Providers } from "@/app/providers"
 import { cn } from "@/lib/utils"
 import { Inter } from "next/font/google"
 

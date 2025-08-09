5199d86112e2c814dfa742128521f842dcd4ca32
Drew ✦ Andersen <drew@monsterswithink.com>
2025-08-09 16:52:38 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,19 @@
 export { auth as middleware } from "@/lib/auth"
+
+export default auth({
+  callbacks: {
+    authorized({ req, token }) {
+      return !!token
+    },
+  },
+})
+
+export const config = {
+  matcher: [
+    "/profile/:path*",
+    "/resume/:path*",
+    "/editor",
+    "/editor/:path*",
+    // add others as needed
+  ],
+}

0bb7f0384fd73c426a1a2a387aa85d8aed6c0728
Drew ✦ Andersen <drew@monsterswithink.com>
2025-08-13 09:15:06 -0600

    Update SignInButton.tsx

--- components/SignInButton.tsx
+++ components/SignInButton.tsx
@@ -2,7 +2,17 @@
 
 import { signIn } from "next-auth/react"
 import { Button } from "@/ui/button"
- 
+
 export default function SignIn() {
-  return <Button onClick={() => signIn("linkedin")} />
+  return (
+    <Button
+      onClick={() =>
+        signIn("linkedin", {
+          callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/profile`
+        })
+      }
+    >
+      Sign in with LinkedIn
+    </Button>
+  )
 }

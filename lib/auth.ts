11ddc367c5461b0f19e0c1502c5f41188534287f
Drew ✦ Andersen <drew@monsterswithink.com>
2025-08-14 19:12:07 -0600

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -10,7 +10,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       clientId: process.env.LINKEDIN_CLIENT_ID,
       clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
       authorization: {
-        params: { scope: "openid profile email" },
+        params: { scope: "r_liteprofile email" },
       },
       profile: (profile) => {
         return {

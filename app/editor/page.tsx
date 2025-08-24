c5591f1d0de89b8859ac3c68ab7ec4765848bf89
Drew ✦ Andersen <drew@monsterswithink.com>
2025-08-24 06:42:11 -0600

    Update page.tsx

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,12 +1,10 @@
-'use client'
+"use client";
 
 import dynamic from "next/dynamic";
 
-// Disable SSR for this page
-const ResumeEditor = dynamic(
-  () => import("@/resume-blocks/ResumeEditor").then((mod) => mod.ResumeEditor),
-  { ssr: false }
-);
+const ResumeEditor = dynamic(() => import("@/resume-blocks/ResumeEditor"), {
+  ssr: false,
+});
 
 export default function ResumePage() {
   return <ResumeEditor />;

d55274bc5b18a41539d1ec3ff4c0bad20832dd05
Drew Andersen <drew@monsterswithink.com>
2025-08-25 04:47:05 +0000

    provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,20 +1,24 @@
-'use client'
+"use client";
 
-import { SessionProvider } from 'next-auth/react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes'
-import { Toaster } from '@/components/ui/sonner'
-import { VeltProvider } from "@/components/resume-providers/Velt"
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes";
+import React from "react";
+import { Toaster } from "@/components/ui/sonner";
+import { VeltProvider } from "@/components/resume-providers/Velt";
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
     <SessionProvider>
-      <NextThemesProvider attribute="class" defaultTheme="system" enableSystem {...props}>
-        <VeltProvider>
-          {children}
-        </VeltProvider>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        {...props}
+      >
+        <VeltProvider>{children}</VeltProvider>
         <Toaster />
       </NextThemesProvider>
     </SessionProvider>
-  )
+  );
 }

c99464750cb462cf1305471c60f099f50672c7d5
Drew Andersen <drew@monsterswithink.com>
2025-08-25 01:37:06 +0000

    new

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 import dynamic from 'next/dynamic';
+import React from "react";
 
   const AuthErrorContent = dynamic(() => import('@/resume-blocks/AuthErrorContent'), {
       ssr: false,
 });
-  export function AuthErrorPage() {
+  export default function AuthErrorPage() {
     return (
       <AuthErrorContent />
       )
